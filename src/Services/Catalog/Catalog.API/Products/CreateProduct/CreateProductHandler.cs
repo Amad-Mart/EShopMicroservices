fce3b03ca514b2f1ed3f941fe25b31a07cebdd42
Deus Martinez <atw1112b@navitus.com>
2024-12-13 15:36:17 -0500

    Added MediatR

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,5 +1,16 @@
-﻿namespace Catalog.API.Products.CreateProduct;
+﻿using MediatR;
 
-public class CreateProductHandler
+namespace Catalog.API.Products.CreateProduct;
+
+public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
+    : IRequest<CreateProductResult>;
+public record CreateProductResult(Guid Id);
+
+internal class CreateProductHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
 {
+    Task<CreateProductResult> IRequestHandler<CreateProductCommand, CreateProductResult>.Handle(CreateProductCommand request, CancellationToken cancellationToken)
+    {
+        //Business logic to Creatre a product
+        throw new NotImplementedException();
+    }
 }
